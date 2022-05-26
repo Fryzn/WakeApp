30c017c535aca729e2a931fad730c037b7edcb04
Fryzn <jonas.foltin@gmail.com>
2022-05-26 14:50:55 +0200

    Projektdateien hinzufügen.

---
+++ WakeApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace WakeApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region WakeApp-Infos
+            // Autoren: Elias Sahm, Jonas Foltin
+
+            #endregion
+
+        }
+    }
+}
