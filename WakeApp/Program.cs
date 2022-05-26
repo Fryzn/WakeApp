b7ef7ce9e4e89b1f2c6e75505790610febbf83ee
Fryzn <jonas.foltin@gmail.com>
2022-05-26 14:59:20 +0200

    WakeApp-Infos summary mit Anforderungen

--- WakeApp/Program.cs
+++ WakeApp/Program.cs
@@ -12,8 +12,27 @@ namespace WakeApp
         static void Main(string[] args)
         {
             #region WakeApp-Infos
-            // Autoren: Elias Sahm, Jonas Foltin
-
+            ///<summary>
+            ///
+            /// Autoren: Elias Sahm, Jonas Foltin
+            /// Erstelldatum: 26.05.2022
+            /// ---------------------------------
+            /// 
+            /// Anforderungen:
+            ///     1. Planen Sie ihr Vorgehen mit passenden Dokumenten (Klassendiagramm, ERM, ...)
+            ///     2. Entwickeln Sie eine textbasierte Anwendung für die Konsole/das Terminal
+            ///         a.Die App soll zunächst die notwendigen Daten abfragen.
+            ///         (Ankunftszeit/Zielzeit, Dauer des Fahrtweges, benötigte Zeit zum Fertigmachen am Morgen, weitere Verzögerungen)
+            ///         b. Im Anschluss soll die App die Weckzeit berechnen.
+            ///         c. Überlegen Sie sich eine benutzerfreundliche Menüführung. Eine Berechnung kann mehrfach ausgeführt werden.
+            ///         d. Fangen Sie mögliche Fehleingaben ab.
+            ///         e. Testen Sie Ihr Programm mit unterschiedlichen Daten.
+            ///     3. Speichern Sie die eingegebenen Daten in einer Datenbank oder Datei, sodass diese App beim nächsten Start des Programms wieder zur Verfügung stehen.
+            ///     4. Verwenden Sie das Versionskontrollsystem GIT.
+            ///     
+            /// Aufgabenteilung in der Word-Datei "Anforderungen Vorlage" (im Moodle Kurs und im Git-Repository hinterlegt).
+            /// 
+            ///</summary>
             #endregion
 
         }
